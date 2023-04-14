49763c1a9a43c4f646ad2dd0d00e82e70719fb23
nicodenelliGH <ndm9412@gmail.com>
2023-04-14 15:34:52 -0400

    adding path urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name='home'),
+  path('about/', views.about, name='about'),
+  path('animals/', views.animals_index, name='index'),
+  path('animals/<int:animal_id>/', views.animals_detail, name='detail'),
 ]
 
