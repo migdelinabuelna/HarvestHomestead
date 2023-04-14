a0abaeb2e34433094459379db42883c3990090f4
brettschoff <brett.schoff@gmail.com>
2023-04-14 12:22:12 -0600

    Adds first model and connects DB

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Animal(models.Model):
+    name = models.CharField(max_length=50)
+    breed = models.CharField(max_length=50)
+    preferred_living_conditions = models.CharField(max_length=50)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
