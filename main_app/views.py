36c81e7c1b5d4bed75e915601be759afef8a5f49
nicodenelliGH <ndm9412@gmail.com>
2023-04-14 14:48:16 -0400

    adding views

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+# from django.http import HttpResponse
+
+# Define the home view
+def home(request):
+  return render(request,'home.html')
+
+def animals_index(request):
+  pass
+
+def animals_details(request):
+  pass
