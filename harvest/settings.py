94c12e332fb636660f39b9a09a3ed2cab4a64132
nicodenelliGH <ndm9412@gmail.com>
2023-04-20 17:24:59 -0400

    deployment

--- harvest/settings.py
+++ harvest/settings.py
@@ -11,21 +11,42 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import environ
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# this sets all our environment variables to there required
+# values in production if an enivronment variable 
+# is absent, so False, True, True respectively
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False),
+    CSRF_COOKIE_SECURE=(bool, True),
+    SESSION_COOKIE_SECURE=(bool, True),   
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e2+w*u!5)t_6%@zj*8)e-pww59j3d3i_4e=o)#9m*u+47a1xey'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY'] 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
+CSRF_COOKIE_SECURE = env('CSRF_COOKIE_SECURE')
+SESSION_COOKIE_SECURE = env('SESSION_COOKIE_SECURE')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['harvest-homestead.fly.dev', 'localhost']
+CSRF_TRUSTED_ORIGINS = ['https://harvest-homestead.fly.dev']
 
 
 # Application definition
@@ -74,11 +95,11 @@ WSGI_APPLICATION = 'harvest.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# Locally you have to migrate and create a super user again
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'harvest',
-    }
+    "default": dj_database_url.config(
+        default="sqlite:///" + os.path.join(BASE_DIR, "db.sqlite3")
+    )
 }
 
 
@@ -117,6 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = "static"
 
 LOGIN_REDIRECT_URL = '/'
 
